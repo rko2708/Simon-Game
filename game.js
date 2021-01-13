bc4d32438db8b510f2a6f0826aa68e095615e00b
Rahul Kumar Ojha <>
2021-01-13 09:59:16 +0530

    first commit

---
+++ game.js
@@ -0,0 +1,97 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+var userClickedPattern = [];
+
+
+var level = -1;
+
+$(document).keypress(function() {
+  if(level === -1)
+    nextSequence();
+});
+
+
+$(".btn").click(function(){
+  var userChosenColor = $(this).attr("id");
+
+  userClickedPattern.push(userChosenColor);
+
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+
+
+function nextSequence()
+{
+    userClickedPattern = [];
+
+    level++;
+    $("h1").html("Level" + level);
+
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColor);
+}
+
+
+function playSound(name)
+{
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+
+function animatePress(currentColor)
+{
+    $("#" + currentColor).addClass("pressed");
+
+    setTimeout(function(){
+      $("#" + currentColor).removeClass("pressed");
+    }, 100);
+}
+
+
+function startOver()
+{
+    level = -1;
+    gamePattern = [];
+}
+
+
+function checkAnswer(currentLevel)
+{
+  if(userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    // console.log("success");
+
+    if(userClickedPattern.length === gamePattern.length) {
+      setTimeout(function(){
+        nextSequence();
+      }, 1000);
+    }
+
+
+  }
+  else {
+    // console.log("wrong");
+
+    playSound("wrong");
+
+    $("body").addClass("game-over");
+
+    $("h1").html("Game Over, Press Any Key to Restart");
+
+    setTimeout(function(){
+      $("body").removeClass("game-over");
+    }, 200);
+
+    startOver();
+  }
+}
